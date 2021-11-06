9587f570a716c8f7b670a1808244c20f2d4d21f8
Vitaliy Todorov <toxa1002@ex.ua>
2021-11-06 12:01:45 +0200

    поправил div

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -2505,7 +2505,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "22327" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
